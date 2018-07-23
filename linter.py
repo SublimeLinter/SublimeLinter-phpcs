baa8015001df4da8066d41e3e1d8d18bbd6dd56f
Koen Lageveen <koen.lageveen@me.com>
2018-07-23 10:26:52 +0200

    don't lint blade

--- linter.py
+++ linter.py
@@ -6,7 +6,7 @@ class Phpcs(ComposerLinter):
     regex = r'^.*:(?P<line>[0-9]+):(?P<col>[0-9]+): (?:(?P<error>error)|(?P<warning>warning)) - (?P<message>.+)'
     composer_name = 'phpcs'
     defaults = {
-        'selector': 'source.php, text.html.basic',
+        'selector': 'source.php - text.blade, text.html.basic',
         # we want auto-substitution of the filename, but `cmd` does not support that yet
         '--stdin-path=': '${file}'
     }
