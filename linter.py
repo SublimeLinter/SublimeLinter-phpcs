2df49c7786758f44633d7899856be70f63cf9358
Louis Laugesen <louis.laugesen@gmail.com>
2018-12-09 09:08:03 +1100

    Include error and sniff code in error message

--- linter.py
+++ linter.py
@@ -1,9 +1,8 @@
 from SublimeLinter.lint import ComposerLinter
 
-
 class Phpcs(ComposerLinter):
-    cmd = ('phpcs', '--report=emacs', '${args}', '-')
-    regex = r'^.*:(?P<line>[0-9]+):(?P<col>[0-9]+): (?:(?P<error>error)|(?P<warning>warning)) - (?P<message>.+)'
+    cmd = ('phpcs', '--report=csv', '${args}', '-')
+    regex = r'^(?!File).*",(?P<line>[0-9]+),(?P<col>[0-9]+),(?:(?P<error>error)|(?P<warning>warning)),(?P<message>".+",.+),[0-9],[01]'
     composer_name = 'phpcs'
     defaults = {
         'selector': 'source.php - text.blade, text.html.basic',
