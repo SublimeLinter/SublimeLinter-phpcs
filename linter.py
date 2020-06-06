4881ca805d2da6b7bec87981e1f67c5cea017f56
Koen Lageveen <koen.lageveen@me.com>
2020-06-06 22:49:45 +0200

    add -s flag to include error code
    and capture it in the regex

--- linter.py
+++ linter.py
@@ -2,8 +2,8 @@ from SublimeLinter.lint import ComposerLinter
 
 
 class Phpcs(ComposerLinter):
-    cmd = ('phpcs', '--report=emacs', '${args}', '-')
-    regex = r'^.*:(?P<line>[0-9]+):(?P<col>[0-9]+): (?:(?P<error>error)|(?P<warning>warning)) - (?P<message>.+)'
+    cmd = ('phpcs', '-s', '--report=emacs', '${args}', '-')
+    regex = r'^.*:(?P<line>[0-9]+):(?P<col>[0-9]+): (?:(?P<error>error)|(?P<warning>warning)) - (?P<message>.+) \((?P<code>.*)\)'
     defaults = {
         'selector': 'source.php - text.blade, text.html.basic',
         # we want auto-substitution of the filename, but `cmd` does not support that yet
