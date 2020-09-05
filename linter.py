9bfdf22f2c71e530f953a4acf9e6aca1f365ac8d
Koen Lageveen <braver@users.noreply.github.com>
2020-09-05 21:25:09 +0200

    capture error code if the -s arg is used (#51)

--- linter.py
+++ linter.py
@@ -3,9 +3,10 @@ from SublimeLinter.lint import ComposerLinter
 
 class Phpcs(ComposerLinter):
     cmd = ('phpcs', '--report=emacs', '${args}', '-')
-    regex = r'^.*:(?P<line>[0-9]+):(?P<col>[0-9]+): (?:(?P<error>error)|(?P<warning>warning)) - (?P<message>.+)'
+    regex = r'^.*:(?P<line>[0-9]+):(?P<col>[0-9]+): (?:(?P<error>error)|(?P<warning>warning)) - (?P<message>(.(?!\(\S+\)$))+)( \((?P<code>\S+)\)$)?'  # noqa: E501
     defaults = {
         'selector': 'source.php - text.blade, text.html.basic',
-        # we want auto-substitution of the filename, but `cmd` does not support that yet
+        # we want auto-substitution of the filename,
+        # but `cmd` does not support that yet
         '--stdin-path=': '${file}'
     }
