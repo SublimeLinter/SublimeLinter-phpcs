e356ce2c6fc6a3383a4ab8f7eea1ecb3ef7aa978
herr kaste <herr.kaste@gmail.com>
2022-08-12 17:20:19 +0200

    Update selector for Sublime Text >= 4134

--- linter.py
+++ linter.py
@@ -5,7 +5,7 @@ class Phpcs(ComposerLinter):
     cmd = ('phpcs', '--report=emacs', '${args}', '-')
     regex = r'^.*:(?P<line>[0-9]+):(?P<col>[0-9]+): (?:(?P<error>error)|(?P<warning>warning)) - (?P<message>(.(?!\(\S+\)$))+)( \((?P<code>\S+)\)$)?'  # noqa: E501
     defaults = {
-        'selector': 'source.php - text.blade, text.html.basic',
+        'selector': 'embedding.php, source.php  - text.blade',
         # we want auto-substitution of the filename,
         # but `cmd` does not support that yet
         '--stdin-path=': '${file}'
