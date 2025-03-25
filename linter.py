8a9b63baa92d571dea2254cc7ea36b929a066f2e
Koen Lageveen <koen.lageveen@me.com>
2025-03-25 10:00:08 +0100

    use checktyle report for more detail
    get access to the error code, missing from the emacs output
    also make the quick action work

--- linter.py
+++ linter.py
@@ -2,8 +2,8 @@ from SublimeLinter.lint import ComposerLinter
 
 
 class Phpcs(ComposerLinter):
-    cmd = ('phpcs', '--report=emacs', '${args}', '-')
-    regex = r'^.*:(?P<line>[0-9]+):(?P<col>[0-9]+): (?:(?P<error>error)|(?P<warning>warning)) - (?P<message>(.(?!\(\S+\)$))+)( \((?P<code>\S+)\)$)?'  # noqa: E501
+    cmd = ('phpcs', '--report=checkstyle', '${args}', '-')
+    regex = r'^\s*<error line="(?P<line>\d+)" column="(?P<col>\d+)" severity="(?:(?P<error>error)|(?P<warning>warning))" message="(?P<message>[^"]+)" source="(?P<code>[^"]+)"'  # noqa: E501
     defaults = {
         'selector': 'embedding.php, source.php  - text.blade',
         # we want auto-substitution of the filename,
