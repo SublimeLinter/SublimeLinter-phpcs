4a9203c3501660c357550082de018d82656bc526
Koen Lageveen <koen.lageveen@me.com>
2025-03-25 11:47:59 +0100

    use the CSV format
    issue: quotes are escaped in the messages

--- linter.py
+++ linter.py
@@ -1,12 +1,23 @@
-from SublimeLinter.lint import ComposerLinter
+import csv
+from io import StringIO
+from SublimeLinter.lint import LintMatch, ComposerLinter
 
 
 class Phpcs(ComposerLinter):
-    cmd = ('phpcs', '--report=checkstyle', '${args}', '-')
-    regex = r'^\s*<error line="(?P<line>\d+)" column="(?P<col>\d+)" severity="(?:(?P<error>error)|(?P<warning>warning))" message="(?P<message>[^"]+)" source="(?P<code>[^"]+)"'  # noqa: E501
+    cmd = ('phpcs', '--report=csv', '${args}', '-')
     defaults = {
         'selector': 'embedding.php, source.php  - text.blade',
         # we want auto-substitution of the filename,
         # but `cmd` does not support that yet
         '--stdin-path=': '${file}'
     }
+
+    def find_errors(self, output):
+        for match in csv.DictReader(StringIO(output)):
+            yield LintMatch(
+                line=int(match.get('Line', 1)) - 1,
+                col=int(match.get('Column', 1)) - 1,
+                error_type=match.get('Type', 'warning'),
+                code=match.get('Source', ''),
+                message=match.get('Message', ''),
+            )
